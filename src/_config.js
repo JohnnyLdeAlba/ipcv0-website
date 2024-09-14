0cd7f700b070dec2b02ee5500344dceb14b74861
zaiphirion <zaiphirion@zaiphirion.zaiphirion>
2024-09-14 13:51:37 -0700

    Added ignore directive for config.

--- src/_config.js
+++ src/_config.js
@@ -1,9 +1,35 @@
+import { createEngLang } from "./lang";
+
 class t_config {
 
   providerURI;
+  defaultChainId;
+  sourceContract;
+  wrapperContract;
+  loadSegmentSize;
+  lang;
 
   constructor() {
-    this.providerURI = "";
+
+    this.developerMode = false;
+
+    if (this.developerMode == true) {
+
+      this.providerURI = "http://127.0.0.1:8545";
+      this.defaultChainId = "0x29a";
+      this.sourceContract = "0x6dA606FA936f89A1626b5AAE64a95bE47E5c2F03";
+      this.wrapperContract = "0x1EE78D4eae774ABBfce59571ED6D410C9670C431";
+    }
+    else {
+
+      this.providerURI = "";
+      this.defaultChainId = "0x1";
+      this.sourceContract = "0x4787993750B897fBA6aAd9e7328FC4F5C126e17c";
+      this.wrapperContract = "0x61E1eA57558B81c3B62578901502fe74D5a924c1";
+    }
+
+    this.loadSegmentSize = 50;
+    this.lang = createEngLang();
   }
 };
 
